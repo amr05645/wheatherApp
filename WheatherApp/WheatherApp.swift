21d1c52295ffb6f65159b59e840593b6652f2f85
amr05645 <amrdawoud702@gmail.com>
2023-12-28 14:27:20 +0200

    rename main app file

---
+++ WheatherApp/WheatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheatherAppApp.swift
+//  WheatherApp
+//
+//  Created by Amr Hassan on 28/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct WheatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
